12465834caeac9a5fc60ae33ecf4a693a08092ec
David Cloutier <algritz@videotron.ca>
2012-06-07 16:47:23 -0400

    more work

--- app/models/recette.rb
+++ app/models/recette.rb
@@ -1,3 +1,8 @@
 class Recette < ActiveRecord::Base
   attr_accessible :author_id, :category_id, :cooking_time, :ingredient, :instruction, :marinade_time, :note, :portion, :preparation_time, :source, :suggested_meal, :suggested_wine, :tip, :title
+  validates :title, :presence => true, :length => 6 .. 128
+  validates :author_id, :presence => true
+  validates :category_id, :presence => true
+  validates :ingredient, :presence => true
+  validates :instruction, :presence => true
 end
--- spec/models/recette_spec.rb
+++ spec/models/recette_spec.rb
@@ -1,8 +1,48 @@
 require 'spec_helper'
 
 describe Recette do
- it "Should have a title" do
-   go "index"
-   response.should have_selector("title")
- end
+
+# Recette.new(:title => "Une Recette", :author_id => 1, :category_id => 1, :ingredient => "test ingredients", :instruction => "test instruction")
+
+  it "should not allow null title" do
+    la_recette = Recette.new(:author_id => 1, :category_id => 1, :ingredient => "test ingredients", :instruction => "test instruction")
+    la_recette.title = nil
+    la_recette.should_not be_valid
+  end
+
+  it "should have a title defined" do
+    la_recette = Recette.new(:title => "Une Recette", :author_id => 1, :category_id => 1, :ingredient => "test ingredients", :instruction => "test instruction")
+    la_recette.should be_valid
+  end
+
+  it "should not allow short title" do
+    la_recette = Recette.new(:title => "Une", :author_id => 1, :category_id => 1, :ingredient => "test ingredients", :instruction => "test instruction")
+    la_recette.should_not be_valid
+  end
+  
+  it "should not allow long title" do
+    la_recette = Recette.new(:title => "a"*256, :author_id => 1, :category_id => 1, :ingredient => "test ingredients", :instruction => "test instruction")
+    la_recette.should_not be_valid
+  end
+  
+  it "should have an author defined" do
+    la_recette = Recette.new(:title => "Une Recette", :author_id => nil, :category_id => 1, :ingredient => "test ingredients", :instruction => "test instruction")
+    la_recette.should_not be_valid
+  end
+  
+  it "should have a category defined" do
+    la_recette = Recette.new(:title => "Une Recette", :author_id => 1, :category_id => nil, :ingredient => "test ingredients", :instruction => "test instruction")
+    la_recette.should_not be_valid
+  end
+  
+  it "should have an ingredient defined" do
+    la_recette = Recette.new(:title => "Une Recette", :author_id => 1, :category_id => 1, :ingredient => nil, :instruction => "test instruction")
+    la_recette.should_not be_valid
+  end
+  
+  it "should have an instruction defined" do
+    la_recette = Recette.new(:title => "Une Recette", :author_id => 1, :category_id => 1, :ingredient => "test instruction", :instruction => nil)
+    la_recette.should_not be_valid
+  end
+  
 end
